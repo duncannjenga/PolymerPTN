2611566c98872c3d830a91bf8b921bf92cf1d70b
justsimplepedro <34727528+justsimplepedro@users.noreply.github.com>
2018-09-29 11:18:26 +0800

    change email provider

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
     'db': 'mongodb://admin:admin@ds143907.mlab.com:43907/ptn_availability',
     // 'db': 'mongodb://pedro:villatumabiene_14@ds235431.mlab.com:35431/ptn_availabilitylocal',
     'secret': 'my secret',
-    'email': 'hirokiyokota1@gmail.com',
-    'pass': 'hiroki123456'
+    'email': 'ptnsystem01@gmail.com',
+    'pass': 'PtnSystem_01'
 }
